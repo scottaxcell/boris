a96c2ac653ac5a7b5fbd4fa9c20c1ac4f656de27
coherentchaos <scott.axcell@gmail.com>
2018-12-22 05:27:33 -0700

    adding threading example to client testcases

--- com.axcell.boris.client/src/test/java/com/axcell/boris/client/GdbDebugClientTest.java
+++ com.axcell.boris.client/src/test/java/com/axcell/boris/client/GdbDebugClientTest.java
@@ -6,6 +6,8 @@ import org.eclipse.lsp4j.debug.Capabilities;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 public class GdbDebugClientTest {
@@ -14,14 +16,28 @@ public class GdbDebugClientTest {
     private static final int ONE_SECOND = 1000;
     private static final int HALF_SECOND = 500;
 
-    private static final String TEST_CASE_DIR = "/home/saxcell/dev/boris/testcases/helloworld";
-    private static final String SOURCE_FILENAME = String.format("%s/helloworld.cpp", TEST_CASE_DIR);
-    private static final String TARGET_FILENAME = String.format("%s/helloworld", TEST_CASE_DIR);
+    private static final String[] makeCmdLine = {"make", "-f", "makefile"};
+    private final String TEST_CASE_DIR = this.getClass().getResource("/threadexample").getPath();
+    private final String SOURCE_FILENAME = this.getClass().getResource("/threadexample/threadexample.cpp").getPath();
+    private final String TARGET_FILENAME = String.format("%s/threadexample", TEST_CASE_DIR);
     private Target target = new Target(TARGET_FILENAME);
+
     private GlobalBreakpointMgr globalBreakpointMgr = GlobalBreakpointMgr.getInstance();
+
+    private void compileThreadExampleExe() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(makeCmdLine);
+            processBuilder.directory(new File(TEST_CASE_DIR));
+            processBuilder.start();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @org.junit.Before
     public void setUp() throws Exception {
+        compileThreadExampleExe();
     }
 
     @org.junit.After
